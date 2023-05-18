29f757937e4380f59d1f42cce5e468b150850ad5
Sam Johnson <sam@durosoft.com>
2023-05-18 10:40:33 -0400

    scaffolded some more syntax, not implemented yet

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,23 @@ quote_parse!(MyThing,
 
     #{Visibility: viz} struct #struct_ident {
         #field1: #{field1_type: Type},
-        #field2: #{field2_type: Type},
         fixed: usize,
+        // each thing in #!{..} is optional but the presence of the first token requires all
+        // subsequent tokens except those nested inside an #?{..}. All tokens inside are
+        // generated as Option fields
+        // #?{..} is optional
+        #!{#field2: #{field2_type: Type} #?{field2_comma: Token![,]}} // note this comma is optional, but field3 will only be parsed if this particular comma is present
+        #? if field2_comma.is_some() {
+            // conditional branches have the same behavior as #?{..} in terms of tokens being auto-optioned
+            #field3: #{field3_type: Type}
+        } else {
+            // if field2 is missing, we parse this `fixed2` as the 4th field
+            fixed2: #{fixed2_type: TypePath}
+        }
+    }
+
+    // the magic variable `$parser` can be used to dispatch methods to the underlying `ParseStream`
+    #? if $parser.peek(Ident) {
+        #{goodbye: Option<keywords::goodbye>}
     }
 );
